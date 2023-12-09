6e4c18fab89a39b75cea7469fac3b8a2e11e8f0b
sundehakon <142574795+sundehakon@users.noreply.github.com>
2023-12-09 13:56:20 +0100

    added reference, fixed user input variables

--- script.cs
+++ script.cs
@@ -11,10 +11,15 @@ namespace YoutubeDownloader
         static async Task Main(string[] args)
         {
             var youtube = new YoutubeClient();
-            Console.Write("Enter the YouTube video ID, this can be found in the link after the "v ="");
-            string video-id = Console.ReadLine();
-            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video-id);
-            var streamInfo = streamManifest
+            Console.Write("Enter the YouTube video ID, this can be found in the link after the 'v =': ");
+            string videoid = Console.ReadLine();
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoid);
+            var streamInfo = streamManifest.GetAudioOnly().WithHighestBitrate();
+            Console.Write("Enter video title: ");
+            string videoTitle = Console.ReadLine();
+            var fileExtension = streamInfo.Container.GetFileExtension();
+            var output = Path.Combine(Path.GetTempPath(), $"{videoTitle}.{fileExtension}");
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, output);
         }
     }
 }
